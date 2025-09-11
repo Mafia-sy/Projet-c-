0043d4c0e4160671924d73e347d6b9fd325baa11
ISMAIL <ahmadbarakat169@gmail.com>
2025-09-11 11:04:45 +0200

    conducteur ok testé

--- conducteur.h
+++ conducteur.h
@@ -3,33 +3,25 @@
 
 #include <iostream>
 #include <string>
-#include "Moto.h"   // Ajout pour utiliser la classe Moto
 
 class Conducteur
 {
 public:
-    // Constructeurs
+    // Constructeur par défaut
     Conducteur();
-    Conducteur(std::string nom, std::string prenom, int anneeNaissance);
 
     // Destructeur
     ~Conducteur();
 
+    // Getters
     std::string getNom();
     std::string getPrenom();
     int getAnneeNaissance();
-
-    // Gestion des motos
-    void afficheMotos();
-    void addMoto(Moto newMoto);
-    void rmMoto(Moto rmMoto);
 
 private:
     std::string nom;
     std::string prenom;
     int anneeNaissance;
-    Moto mesMotos[10];  // Tableau fixe de 10 motos
-    int nbMotos;        // Nombre de motos ajoutées
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,34 +1,18 @@
 #include <iostream>
 #include "conducteur.h"
-#include "Moto.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello World !" << endl;
 
-    // Conducteurs
+    // Création d’un conducteur (constructeur par défaut)
     Conducteur c1;
-    Conducteur c2("ISMAIL", "BARAKAT", 1990);
 
-    // Motos
-    Moto m1("Yamaha R1");
-    Moto m2("Kawasaki Ninja");
-
-    // Ajout de motos
-    c2.addMoto(m1);
-    c2.addMoto(m2);
-
-    // Affichage
-    cout << "Nom : " << c2.getNom()
-        << ", Prénom : " << c2.getPrenom()
-        << ", Année de naissance : " << c2.getAnneeNaissance() << endl;
-
-    c2.afficheMotos();
-
-    // Suppression d’une moto
-    c2.rmMoto(m1);
-    c2.afficheMotos();
+    // Utilisation des getters
+    cout << "Nom : " << c1.getNom()
+        << ", Prénom : " << c1.getPrenom()
+        << ", Année de naissance : " << c1.getAnneeNaissance() << endl;
 
     return 0;
 }
