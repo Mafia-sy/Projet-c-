d442c748008d372eda4d06b5c9f952441681e461
Noah <noahkersten17@gmai.com>
2025-09-11 10:12:14 +0200

    ajout des fichiers

---
+++ Moto.h
@@ -0,0 +1,22 @@
+#ifndef MOTO_H
+#define MOTO_H
+#include <iostream>
+#include <string>
+
+class Moto
+{
+public:
+    Moto();
+    ~Moto();
+    std::string 
+
+
+private:
+    int poids;
+    Moteur moteur;
+
+
+
+
+};
+#endif
