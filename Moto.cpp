5e745b0751520e654c66356521899aa89cf8c793
Noah <noahkersten17@gmai.com>
2025-09-12 09:55:18 +0200

    maj Moto.h et Moto.cpp

---
+++ Moto.cpp
@@ -0,0 +1,30 @@
+#include "Moto.h"
+
+// Constructeur par défaut
+Moto::Moto() : poids(0), moteur() {}
+
+// Destructeur
+Moto::~Moto() {
+    std::cout << "Moto détruite" << std::endl;
+}
+
+// Getter Poids
+int Moto::getPoids() {
+    return poids;
+}
+
+// Setter Poids
+void Moto::setPoids(int p) {
+    poids = p;
+}
+
+// Getter Moteur
+Moteur Moto::getMoteur() {
+    return moteur;
+}
+
+// Setter Moteur
+void Moto::setMoteur(Moteur m) {
+    moteur = m;
+}
+
--- Moto.h
+++ Moto.h
@@ -1,22 +1,26 @@
 #ifndef MOTO_H
 #define MOTO_H
-#include <iostream>
-#include <string>
 
-class Moto
-{
+#include <iostream>
+#include "Moteur.h"
+
+class Moto {
 public:
+    // Constructeur et destructeur
     Moto();
     ~Moto();
-    std::string 
 
+    // Getters
+    int getPoids();
+    Moteur getMoteur();
+
+    // Setters
+    void setPoids(int p);
+    void setMoteur(Moteur m);
 
 private:
     int poids;
     Moteur moteur;
-
-
-
-
 };
+
 #endif
