23fc56bf16194fed30318dbedcff2f33597b8793
ISMAIL <ahmadbarakat169@gmail.com>
2025-09-12 10:17:33 +0200

    modification fichier main testé

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Moteur.h"
 #include "Electrique.h"
 #include "Thermique.h"
+#include "Moto.h"
 
 using namespace std;
 
@@ -11,7 +12,6 @@ int main() {
 
     // --- Test conducteur ---
     Conducteur c1;
-
     cout << "Nom : " << c1.getNom()
         << ", Prénom : " << c1.getPrenom()
         << ", Année de naissance : " << c1.getAnneeNaissance() << endl;
@@ -19,7 +19,6 @@ int main() {
     // --- Test moteur générique ---
     Moteur m1;
     cout << "Puissance actuelle : " << m1.getPuissance() << " ch" << endl;
-
     m1.setPuissance(150);
     cout << "Nouvelle puissance : " << m1.getPuissance() << " ch" << endl;
 
@@ -27,7 +26,6 @@ int main() {
     Electrique me;
     me.setPuissance(120);
     me.setTensionMax(400.5f);
-
     cout << "[Electrique] Puissance : " << me.getPuissance()
         << " ch, Tension max : " << me.getTensionMax() << " V" << endl;
 
@@ -35,9 +33,17 @@ int main() {
     Thermique mt;
     mt.setPuissance(200);
     mt.setCylindree(2.0f);
-
     cout << "[Thermique] Puissance : " << mt.getPuissance()
         << " ch, Cylindrée : " << mt.getCylindree() << " L" << endl;
+
+    // --- Test Moto ---
+    Moto moto1;
+    moto1.setPoids(180);
+    moto1.setMoteur(m1);
+
+    cout << "[Moto] Poids : " << moto1.getPoids()
+        << " kg, Puissance moteur : " << moto1.getMoteur().getPuissance()
+        << " ch" << endl;
 
     return 0;
 }
